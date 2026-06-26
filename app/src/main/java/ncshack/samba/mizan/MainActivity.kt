5482d40460f12e1e67788f0260f6fdff16685f79
Younes Bouhouche <younes.bouhouche12@gmail.com>
2026-06-26 14:49:27 +0100

    feat: Add app icon and splash screen quick animation

--- app/src/main/java/ncshack/samba/mizan/MainActivity.kt
+++ app/src/main/java/ncshack/samba/mizan/MainActivity.kt
@@ -5,22 +5,42 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import ncshack.samba.mizan.navigation.NavGraph
+import ncshack.samba.mizan.presentation.viewmodel.AuthViewModel
 import ncshack.samba.mizan.ui.theme.MizanTheme
+import org.koin.compose.viewmodel.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MizanTheme {
-
+                MizanAppContent()
             }
         }
+    }
+}
+
+@Composable
+fun MizanAppContent() {
+    val navController = rememberNavController()
+    val authViewModel: AuthViewModel = koinViewModel()
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        NavGraph(
+            navController = navController,
+            authViewModel = authViewModel,
+        )
     }
 }
